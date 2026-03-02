c2abdd731c5e6bbbe8a4252fe939fe5fad66759b
abduvoris0408 <abduvorismuminov0408@gmail.com>
2026-03-02 12:47:13 +0500

    Eski loyiha tozalandi

--- data/site.ts
+++
@@ -1,9 +0,0 @@
-// Site navigation data
-
-export const navigation = [
-  { name: "Bosh sahifa", href: "/" },
-  { name: "Haqida", href: "/about" },
-  { name: "Xizmatlar", href: "/practice" },
-  { name: "Mediatsiya", href: "/mediation" },
-  { name: "Yangiliklar", href: "/news" },
-]
